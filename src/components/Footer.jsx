670415b3025cc9439b7ec67ad4f26c7e743c8ab4
maria <malubf20@gmail.com>
2026-03-30 19:03:49 -0300

    feat:Adicionando o texto do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,38 @@
+import '../css/global.css'
+import '../css/footer.css'
+import { BsInstagram, BsWhatsapp, BsGeoAlt, BsEnvelope } from 'react-icons/bs'
+
 const Footer = () => {
   return (
-    <section>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-info">
+          <h4>Contato</h4>
+
+          <p>
+            <BsEnvelope className="footer-icon" />
+            glowgym@gmail.com
+          </p>
+
+          <p>
+            <BsWhatsapp className="footer-icon" />
+            (11) 99999-9999
+          </p>
+
+          <p>
+            <BsInstagram className="footer-icon" />
+            @glowgym
+          </p>
+
+          <p>
+            <BsGeoAlt className="footer-icon" />
+            Av. Paulista, 1000 - São Paulo, SP
+          </p>
+        </div>
+
         <h6>&copy; 2026 - direitos reservados</h6>
-    </section>
+      </div>
+    </footer>
   )
 }
 
